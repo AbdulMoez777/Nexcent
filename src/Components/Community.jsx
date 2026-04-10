18bccbba401cee1879fa7e2bfa3139d8ee173e0f
Abdul Moez <abdulmoeztwitter@gmail.com>
2026-04-10 20:10:17 +0500

    5:  Community Section (Adding Member Cards ) Completed

--- src/Components/Community.jsx
+++ src/Components/Community.jsx
@@ -11,6 +11,56 @@ function Community() {
           </h2>
           <p className="text-grey text-lg">Who is Nexcent suitable for?</p>
         </div>
+
+        <div className="flex flex-col md:flex-row justify-between gap-8">
+          {/* Card1  */}
+          <div className="flex flex-col items-center px-6 py-8 bg-white rounded-lg shadow-sm border border-gray-100 md:w-1/3">
+            <img
+              src="public\Images\Icon1.png"
+              alt="Membership"
+              className="h-14 mb-4"
+            />
+            <h3 className="text-3xl font-bold text-d-grey mb-3">
+              Membership <br /> Organisations
+            </h3>
+            <p className="text-grey text-sm">
+              Our membership management software provides full automation of
+              membership renewals and payments
+            </p>
+          </div>
+
+          {/* Card2  */}
+          <div className="flex flex-col items-center px-6 py-8 bg-white rounded-lg shadow-sm border border-gray-100 md:w-1/3">
+            <img
+              src="public\Images\Icon2.png"
+              alt="Membership"
+              className="h-14 mb-4"
+            />
+            <h3 className="text-3xl font-bold text-d-grey mb-3">
+              Membership <br /> Organisations
+            </h3>
+            <p className="text-grey text-sm">
+              Our membership management software provides full automation of
+              membership renewals and payments
+            </p>
+          </div>
+
+          {/* Card3  */}
+          <div className="flex flex-col items-center px-6 py-8 bg-white rounded-lg shadow-sm border border-gray-100 md:w-1/3">
+            <img
+              src="public\Images\Icon3.png"
+              alt="Membership"
+              className="h-14 mb-4"
+            />
+            <h3 className="text-3xl font-bold text-d-grey mb-3">
+              Membership <br /> Organisations
+            </h3>
+            <p className="text-grey text-sm">
+              Our membership management software provides full automation of
+              membership renewals and payments
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
